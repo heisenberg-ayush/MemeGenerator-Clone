a377e1bbf5cee4593c62dc403b3e94559e38e759
Ayush Pandey <73249644+heisenberg-ayush@users.noreply.github.com>
2022-02-14 12:02:35 +0530

    Done

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className='header'>
+        <img className='header--image' src="https://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" alt="Problem?" />
+        <h1 className='header--title'>MEME GENERATOR</h1>
+    </header>
+  )
+}
+
+export default Header
